43cad1d18be84ee571973a4b063e773fb37e5efe
Gabriel Emiliano Gonzalez Camacho <gavodx@hotmail.com>
2026-06-17 18:40:09 -0600

    Update login.php

--- backend/login.php
+++ backend/login.php
@@ -3,7 +3,7 @@ require 'config.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data = json_decode(file_get_contents("php://input"), true);
-    
+
     $usuario = $data['usuario'] ?? null;
     $clave = $data['clave'] ?? null;
 
